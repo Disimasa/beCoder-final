afbbf031ad0a781e8dd5e33d642f8e0d8616baed
Dikower <dmitriy1d01@gmail.com>
2020-07-30 19:10:35 +0300

    Добавил страницу Семена

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -52,6 +52,7 @@
 	<ul>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
 		<li><a aria-current="{segment === 'editor' ? 'page' : undefined}" href="editor">Редактор</a></li>
+		<li><a aria-current="{segment === 'semyon' ? 'page' : undefined}" href="semyon">Страница Семена</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
