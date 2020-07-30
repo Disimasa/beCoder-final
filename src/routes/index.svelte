47430342b4c0f4c2cd23f83951574c22315c71ce
Dikower <dmitriy1d01@gmail.com>
2020-07-30 19:03:55 +0300

    Добавил страницу Семена

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,84 @@
+<script>
+let projects = [
+{name: "Project_1", text: "Great!",onclick:"alert('Project_1');", class_:"alternate"},
+{name: "Project_2", text: "Nice!",onclick:"alert('Project_2');", class_:"alternate"},
+{name: "Project_3", text: "Wonder!",onclick:"alert('Project_3');", class_:"alternate"}
+];
+</script>
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  section {
+    padding-top: 15vh;
+    padding-bottom: 60vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    background-color: #000000;
+    border: solid 0.01px #016220;
+    box-shadow: 2px 2px 6px rgb(0, 0, 0);
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  div {
+    margin: 2vh;
+    padding: 2vh;
+    height: 30vh;
+    width: 30vh;
+    color: white;
+    text-align: center;
+    min-width: 30vh;
+    min-height: 20vh;
+    box-shadow: 2px 2px 6px rgb(0, 0, 0);
+    border-radius: 1rem;
+    border: solid 0.01px #016220;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  div a {
+    background-color: #1e2122;
+    color: #016220;
+    text-decoration: none;
+    width: 5rem;
+    margin: 0 auto;
+    box-shadow: 2px 2px 6px #01A220;
+    border-radius: 5px;
+    border: solid 0.01px #01B220;
+    padding: 0.3rem;
+    cursor: pointer;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  div a:hover {
+    background-color: white;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  .primary {
+    background-color: black;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  .alternate {
+    background-color: #1e2122;
+  }
+  .new {
+    background-color: #016220;
+
+  }
+
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
+<section id="projects">
 
-<h1>Great success!</h1>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+{#each projects as { name, text, onclick, class_ }}
+  <div class={class_} onclick={onclick}>
+    <h1>{name}</h1>
+    <p style="font-size:3vh">{text}</p>
+  </div>
+{/each}
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+    <div class="new" onclick="document.location.href = 'http://ya.ru'" >
+    <p style="font-size:10vh">➕</p>
+  </div>
+
+
+</section>
