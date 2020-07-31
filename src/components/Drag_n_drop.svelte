1634cafcc11796bbc4336d7b0b85f052c3bad126
Disimasar <lichko2002@mail.ru>
2020-07-31 08:59:59 +0300

    new_version

--- src/components/Drag_n_drop.svelte
+++ src/components/Drag_n_drop.svelte
@@ -1,38 +1,74 @@
 <script>
 import { onMount } from "svelte";
 import interact from "interactjs";
+let chosen_block = null;
 let hidden = false;
+let count = 0;
+let elements = [];
+function new_arrow(target) {
+}
+function delete_block(e) {
+    console.log(elements);
+    if (chosen_block != null && (e.keyCode===8 || e.keyCode === 46)) {
+        elements.forEach((el, index) => {
+           if (chosen_block === el['id']) {
+               elements.splice(index, 1);
+           }
+        });
+        elements = elements;
+        chosen_block = null;
+    }
+}
 function open_input(event) {
-    console.log();
     if (event.clientX > 200 && event.target.childElementCount === 0) {
         let input = document.createElement('input');
+        input.classList.add('input_title');
         event.target.append(input);
+
     }
+}
+function new_block(type) {
+    elements = [...elements, {'id': count, 'x': 500, 'y':500, 'type': type}];
+        count+=1;
+}
+function update() {
+    elements.forEach((el) => {
+       let item = document.getElementById(el['id']);
+       item.style.top = el['y']+'px';
+       item.style.left = el['x']+'px';
+    });
 }
 onMount(() => {
     interact('.item')
     .resizable({
-    edges: { left: true, right: true, bottom: true, top: true },
+    edges: { left: false, right: true, bottom: false, top: true },
     listeners: {
+        start(event) {
+
+        },
       move (event) {
-        let target = event.target;
-        let x = (parseFloat(target.getAttribute('data-x')) || 0);
-        let y = (parseFloat(target.getAttribute('data-y')) || 0);
+              let target = event.target;
+              let x = (parseFloat(target.getAttribute('data-x')) || 0);
+              let y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-        target.style.width = event.rect.width + 'px';
-        target.style.height = event.rect.height + 'px';
+              target.style.width = event.rect.width + 'px';
+              target.style.height = event.rect.height + 'px';
 
-        x += event.deltaRect.left;
-        y += event.deltaRect.top;
+              x += event.deltaRect.left;
+              y += event.deltaRect.top;
 
-        target.style.webkitTransform = target.style.transform =
-          'translate(' + x + 'px,' + y + 'px)';
+              target.style.webkitTransform = target.style.transform =
+                      'translate(' + x + 'px,' + y + 'px)';
 
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-      }
+              target.setAttribute('data-x', x);
+              target.setAttribute('data-y', y);
+          }
     },
     modifiers: [
+         interact.modifiers.aspectRatio({
+             // make sure the width is always double the height
+             ratio: 'preserve',
+         }),
       interact.modifiers.restrictEdges({
         outer: '.drag_n_drop_area'
       }),
@@ -78,20 +114,26 @@ onMount(() => {
                 target.setAttribute('data-y', y);
             },
             end(event) {
+
             }
         }
     })
 })
 </script>
+<svelte:window on:keydown={delete_block} on:keyup={update}/>
 <div class="component">
     <button class="hide-button" on:click={() => hidden = !hidden}>{hidden ? 'Открыть' : 'Скрыть'}</button>
     <div class="drag_n_drop_area {hidden === false ? 'small_area' : 'big_area'}">
     <div class="collection" class:hidden>
-        <div id="1" class="collection_item rectangle item" on:dblclick={open_input}></div>
-        <div id="2" class="collection_item oval item"></div>
-        <div id="3" class="collection_item circle item"></div>
-        <div id="4" class="collection_item triangle item"></div>
+        <button on:click={new_arrow}></button>
+        <div class="collection_item rectangle" on:mousedown="{() => new_block('rectangle')}" on:mouseup={update}></div>
+        <div class="collection_item oval" on:mousedown="{() => new_block('oval')}" on:mouseup={update}></div>
+        <div class="collection_item circle" on:mousedown="{() => new_block('circle')}" on:mouseup={update}></div>
+        <div class="collection_item triangle" on:mousedown="{() => new_block('triangle')}" on:mouseup={update}></div>
 	</div>
+        {#each elements as el}
+        <div id = {el['id']} class="item {chosen_block === el['id'] ? 'chosen_block':''} {el['type']}" on:mousedown="{() => chosen_block = el['id']}"></div>
+            {/each}
     </div>
 </div>
 <style>
@@ -113,6 +155,9 @@ onMount(() => {
         position: absolute;
         left: 20px;
         margin: 30px 20px;
+    }
+    .collection_item:hover {
+        cursor: pointer;
     }
     .rectangle {
         background: red;
@@ -147,10 +192,10 @@ onMount(() => {
     .drag_n_drop_area {
         position: fixed;
         width:100%;
-        height: 500px;
+        height: 90%;
     }
     .small_area {
-               left: 205px;
+        left: 205px;
     }
     .big_area {
         left:5px;
@@ -158,5 +203,14 @@ onMount(() => {
 
 .item {
     touch-action: none;
+    position: fixed;
+    top: 500px;
+    left: 500px;
 }
+    .nothing {
+        top:200px;
+    }
+    .chosen_block {
+        border: 2px solid brown;
+    }
 </style>
