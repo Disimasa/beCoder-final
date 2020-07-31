e3fcf841cfd040380b045f3be12b22ddc7a7ce28
Dikower <dmitriy1d01@gmail.com>
2020-07-31 13:57:32 +0300

    Исправил страницу Семена

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,32 +6,42 @@
     ];
 </script>
 <style>
-		@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap');
+    .header-h1 {
+        width: 100%;
+        text-align: center;
+        font-family: Comfortaa, sans-serif;
+        font-weight: bold;
+        font-size: 2.5em;
+        color: #525252;
+        padding-bottom: 40px;
+    }
     section {
-			  padding-top: 200px;
+        padding-top: 200px;
         background-color: #FAFAFA;
         width: 100%;
-        height: 100%;
-				top: 0;
+        height: 100vh;
+        top: 0;
     }
 
-    h1 {
+    .title {
         margin: 0;
         padding: 0;
         font-size: 2em;
-				color: #6574FF;
-				font-family: Comfortaa, sans-serif;
-				font-weight: 700;
-		}
+        color: #6574FF;
+        font-family: Comfortaa, sans-serif;
+        font-weight: 700;
+    }
 
     h2 {
-				font-family: Comfortaa, sans-serif;
-				font-weight: 400;
+        font-family: Comfortaa, sans-serif;
+        font-weight: 400;
         margin: 0;
         padding: 0;
         font-size: 1em;
-				color: #858585;
+        color: #858585;
     }
+
     .wrapper {
         width: 100%;
         height: 100%;
@@ -39,6 +49,7 @@
         flex-flow: wrap;
         justify-content: space-around;
     }
+
     .card {
         background: white;
         text-align: center;
@@ -48,13 +59,16 @@
         display: flex;
         flex-direction: column;
         justify-content: space-around;
-				box-shadow: 0 0 30px 2px #EBEBEB;
-				transition: all 1s ease;
-				cursor: pointer;
+        box-shadow: 0 0 30px 2px #EBEBEB;
+        transition: all 1s ease;
+        cursor: pointer;
+        margin-bottom: 40px;
     }
+
     .card:hover {
         box-shadow: 0 0 40px 3px #D6D6D6;
     }
+
     .project {
 
     }
@@ -62,20 +76,22 @@
     .new {
 
     }
-		p {
-			font-size: 40px;
-			color: #525252;
-		}
+
+    p {
+        font-size: 40px;
+        color: #525252;
+    }
 
 </style>
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
 </head>
 <section id="projects">
+    <h1 class="header-h1">Выберите проект или создайте новый</h1>
     <div class="wrapper">
         {#each projects as { name, text, onclick, class_ }}
             <div class="card project" onclick={onclick}>
-                <h1>{name}</h1>
+                <h1 class="title">{name}</h1>
                 <h2>{text}</h2>
             </div>
         {/each}
